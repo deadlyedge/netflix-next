f7d814f9399c3dd3cc6d49693f9243eb62fd4a1b
xdream oldlu <xdream@gmail.com>
2023-12-01 11:00:43 +0800

    done auth backend

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth"
 
-const handler = NextAuth({
-  ...
-})
+import { authOptions } from "@/utils/auth"
+
+const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
